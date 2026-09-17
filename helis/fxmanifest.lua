702c65679f0252feb76c43f2b5cd55729e53ac61
HardyF1 <andrewhardy96@gmail.com>
2026-09-17 21:42:46 +0100

    New Structure Vehicles

---
+++ helis/fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'KillerVehicles'
+description 'KillerVehicles-Civilian-SubPack1'
+version '1.1.0'
+
+files {
+
+	'data/**/handling.meta',
+	'data/**/vehicles.meta',
+	'data/**/carcols.meta',
+	'data/**/carvariations.meta',
+
+
+	
+}
+
+data_file 'HANDLING_FILE'			'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'	'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE'			'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'	'data/**/carvariations.meta'
+
+
+lua54 'yes'
+
+dependency '/assetpacks'
+dependency '/assetpacks'
