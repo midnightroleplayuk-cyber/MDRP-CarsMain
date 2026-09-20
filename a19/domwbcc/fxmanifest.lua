a0876c09188db5ee21286cd3b9b266be3f16a85e
midnightroleplayuk-cyber <midnightroleplayuk@gmail.com>
2026-09-20 12:31:10 +0100

    added cars

---
+++ a19/domwbcc/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'GXD'
+description 'https://gxd-webstore.tebex.io'
+version '1.0.0'
+
+files {
+    '**/**.meta',
+    '**/**.meta',
+    '**/**.meta',
+    '**/**.meta',
+    '**/**.meta',
+}
+
+data_file 'HANDLING_FILE' '**/**.meta'
+data_file 'VEHICLE_METADATA_FILE' '**/**.meta'
+data_file 'CARCOLS_FILE' '**/**.meta'
+data_file 'VEHICLE_VARIATION_FILE' '**/**.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' '**/**.meta'
+
+dependency '/assetpacks'
